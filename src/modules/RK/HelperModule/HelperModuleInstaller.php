9c79c4615b41773929003b03398ec7f21f9966bd
rallek <ralf@familie-koester.de>
2017-04-09 14:31:18 +0200

    update some variables via installer

--- src/modules/RK/HelperModule/HelperModuleInstaller.php
+++ src/modules/RK/HelperModule/HelperModuleInstaller.php
@@ -23,13 +23,20 @@ class HelperModuleInstaller extends AbstractHelperModuleInstaller
 	
 	public function upgrade($oldVersion)
     {
-		
-		case '0.7.2':
-			$this->setVar('descriptionLengthImage', '1000');
-			$this->setVar('descriptionLengthInfoList', '250');
-			$this->setVar('descriptionLengthImageList', '250');
+		switch ($oldVersion) {
+		case '0.7.3':
+        $this->setVar('enableShrinkingForInfoTitleImage', false);
+        $this->setVar('shrinkWidthInfoTitleImage', '800');
+        $this->setVar('shrinkHeightInfoTitleImage', '600');
+        $this->setVar('thumbnailModeInfoTitleImage',  'inset' );
+        $this->setVar('thumbnailWidthInfoTitleImageView', '32');
+        $this->setVar('thumbnailHeightInfoTitleImageView', '24');
+        $this->setVar('thumbnailWidthInfoTitleImageDisplay', '240');
+        $this->setVar('thumbnailHeightInfoTitleImageDisplay', '180');
+        $this->setVar('thumbnailWidthInfoTitleImageEdit', '240');
+        $this->setVar('thumbnailHeightInfoTitleImageEdit', '180');
     
-    
+		}
         // update successful
         return true;
     }
